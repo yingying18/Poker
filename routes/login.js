689e679c359783e5790675c50540b2418c1b38b0
sirfeedsalot <rchhay@mail.sfsu.edu>
2019-04-03 21:37:56 -0700

    Configured package.json for rest of team

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 var mysql = require('mysql');
-//var session = require('express-session');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 
