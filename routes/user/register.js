2dca08ecb358aec1a2606562c3e4cf1510875545
altindag <caltindag@mail.sfsu.edu>
2019-04-11 19:16:57 -0700

    registration backend added with error check

--- routes/user/register.js
+++ routes/user/register.js
@@ -1,15 +1,19 @@
-module.exports = function (app,dbRequest,dbconn) {
+module.exports = function(app, dbRequest, dbconn) {
+  app.get("/register", (req, res) => {
+    console.log("register post called");
 
-	app.get('/register',(req, res) =>{
-		 console.log("register post called");	
-					
-		res.render('user/register');
-			 		
-	});
-	app.post('/registersave',(req, res) =>{
-		console.log("register post called");	
-				   
-	   res.send("ok");
-					
-   });
-}
+    res.render("user/register");
+  });
+
+  app.post("/registersave", (req, res) => {
+    console.log("register post called" + JSON.stringify(req.body));
+    dbRequest.insertUser(dbconn, req.body, function(result) {
+      console.log("return" + JSON.stringify(result));
+      if (typeof result.code !== "undefined" || result === "") {
+        res.send("we encountered an error during the registration");
+      } else {
+        res.send("your registration is done. You can login");
+      }
+    });
+  });
+};
--- utils/dbrequest.js
+++ utils/dbrequest.js
@@ -1,40 +1,61 @@
 module.exports = {
- 
-            insertUser: function(con, data, callback) {
-              // whatever
+  insertUser: function(con, data, callback) {
+    // whatever
 
+    console.log("sql catching the form:" + JSON.stringify(data));
+    var returnValue;
+    //date format 'YYYY-MM-DD'
+    this.resulta = con.query(
+      "insert into user (" +
+        "username," +
+        "password," +
+        "email," +
+        "picture," +
+        "registerdate" +
+        ") values ('" +
+        data.name +
+        "','" +
+        data.password +
+        "','" +
+        data.email +
+        "',0," +
+        "DATE(NOW())" +
+        ")",
+      function(err, result, fields) {
+        if (err) {
+          console.log("error : " + result);
+          callback(err);
+          throw err;
+        } else {
+          console.log("in sendQuery Result: " + result);
+          callback(result);
+        }
+        //con.release();
+      }
+    );
+  },
+  getUsers: function(con, data, callback) {
+    let result = con.query("Select * from user ;", function(
+      err,
+      result,
+      fields
+    ) {
+      if (err) throw err;
+      console.log("in sendQuery Result: " + result[0].id);
+      callback(result);
+      //con.release();
+    });
+  },
 
-                  con.connect(function(err) {
-                    if (err) throw err;
-                    console.log("Connected!");
-                  });
-
-                  //console.log(myvara);
-                  callback('connected');
-
-            },
-            getUsers: function(con,data ,callback) {
-               
-                let result = con.query("Select * from user ;", function (err, result , fields) {
-                   
-                    if (err) throw err;
-                    console.log("in sendQuery Result: " + result[0].id);
-                    callback(result);
-                    //con.release();
-
-                  });
-            },
-
-            updateUser: function(con,data ,callback) {
-                    let result = con.query("update user set username = '"+data.name+"' where id = "+data.id, function (err, result , fields) {
-                   
-                    if (err) throw err;
-                    console.log("in sendQuery Result: " + result);
-                    callback(result);
-                    //con.release();
-
-                    });
-            }
-           
-
+  updateUser: function(con, data, callback) {
+    let result = con.query(
+      "update user set username = '" + data.name + "' where id = " + data.id,
+      function(err, result, fields) {
+        if (err) throw err;
+        console.log("in sendQuery Result: " + result);
+        callback(result);
+        //con.release();
+      }
+    );
+  }
 };
