ce77f43deb7352a0443d3f0bc4193b77db1c5203
altindag <caltindag@mail.sfsu.edu>
2019-04-26 04:27:38 -0700

    callback functions fixed on login logout / lobby game table populated from db

--- routes/game/lobby.js
+++ routes/game/lobby.js
@@ -1,9 +1,17 @@
 module.exports = function (app,dbRequest,dbconn) {
 
 	app.get('/lobby',(req, res) =>{
-		 console.log("lobby called");	
-					
-			 			res.render('game/lobby');
+		console.log("lobby called");
+		dbRequest.getAllTables(dbconn, null, function (result) {
+                  
+                  if (typeof result.code !== "undefined" || result === "") {
+                    res.send("we encountered an error while calling the lobby.");
+                  } else {
+                  	console.log(JSON.stringify(result[1]));
+                    res.render('game/lobby', {result : result, authanticate : req.body.authanticate});
+                  }
+         });	
+		
 			 		
 	});
 
