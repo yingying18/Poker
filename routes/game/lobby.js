20ddeb7330887c82972c19c6fc2ee0ae42bd03b7
Jarek L Rettinghouse <j.rettinghouse@gmail.com>
2019-05-17 23:14:46 -0700

    fixed accidental change

--- routes/game/lobby.js
+++ routes/game/lobby.js
@@ -3,7 +3,7 @@ module.exports = function (app,dbRequest,dbconn) {
 
 	app.get('/lobby',(req, res) =>{
 		console.log("get /lobby called");
-		dbRequest.getAllTables(dbconn, null, function (tablesresult) {
+		dbRequest.getAllTables(dbconn, null, function (result) {
                   
                   if (typeof result.code !== "undefined" || result === "") {
                     res.send("we encountered an error while calling the lobby.");
